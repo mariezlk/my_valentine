f90cb61c3cf82393001e3df08e613b7ff2623dc3
Marie Zielke <marie.zielke@telekom.de>
2026-02-07 23:28:02 +0100

    lovely

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Title, Flex, Button, Box, Text } from "@mantine/core";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box w="100vw" h="100vh" bg="#ff6996" pos="relative">
+      <Flex w="100%" h="100%" justify="center" align="center" direction="column" gap={50}>
+        <Title order={3} c="#ffd3e3">
+          Ich Liebe dich, Jilli!
+        </Title>
+        <Text w="50%" align="center" c="#ffd3e3">
+          ... warum? Wähle eine der folgenden Kategorien
+        </Text>
+        <Button variant="filled" w={130} radius="xl" bg="#ffadc6" c="#fb497a">
+          Spicy
+        </Button>
+        <Button variant="filled" w={130} radius="xl" bg="#ff89a9" c="#ffd3e3">
+          Lovely
+        </Button>
+        <Button variant="filled" w={130} radius="xl" bg="#fb497a" c="#ffadc6">
+          Motivative
+        </Button>
+      </Flex>
+    </Box>
   );
 }
 
